22e315afb1a6b3e57e90a59dfa026219a9080d96
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-08-29 18:07:23 +0900

    jun: flex 속성 수정

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -3,7 +3,7 @@
 	import { PROFILE_MENU } from '$lib/constants/strings';
 </script>
 
-<div class="page-wrapper flex justify-center items-center px-4">
+<div class="page-wrapper flex flex-col justify-center items-center px-4">
 	<h1 class="font-bold">준비중입니다!</h1>
 	<NavBar currentClickedMenu={PROFILE_MENU} />
 </div>
