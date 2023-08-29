bbc1f5f5acf9d26980ddfef45bd9adf4da920638
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-08-29 15:16:04 +0900

    jun: 최외곽 +layout 아닌 +page에서 일단 splash로 이동하도록 수정

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import '../app.css';
-	import { goto } from '$app/navigation';
-
-	onMount(() => {
-		goto('/splash');
-	});
 </script>
 
 <div class="w-full sm:max-w-sm h-screen sm:mx-auto overflow-y-auto bg-white font-sans">
