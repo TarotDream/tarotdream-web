a0cb966c788e7254316c1a643c3af22e0057a09a
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-09-02 23:18:22 +0900

    jun: Toast Component 붙힘 / Image Regeneration API 연동 준비중

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import Toast from '$lib/components/Toast.svelte';
 	import '../app.css';
 </script>
 
 <div class="w-full sm:max-w-sm h-screen sm:mx-auto bg-white font-sans">
 	<slot />
 </div>
+
+<Toast />
