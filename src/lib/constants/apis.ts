833395c5e2e5ed61c67c82de8c9b8170882bc2c8
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-09-02 15:34:26 +0900

    jun: API 목적지 변경

--- src/lib/constants/apis.ts
+++ src/lib/constants/apis.ts
@@ -2,7 +2,7 @@
 export const TAROT_DREAM_DOMAIN = "https://bgremp.com"
 
 // api destination uri
-export const postDreamURI = () => '/dream/generation';
-export const postDreamImageURI = () => '/dream/regeneration';
+export const postDreamURI = () => '/dream/generate';
+export const postDreamImageURI = () => '/dream/regenerate';
 export const getAllDreamsURI = () => '/dream';
 export const getSingleDreamURI = (dreamId:string) => `/dream/${dreamId}`;
