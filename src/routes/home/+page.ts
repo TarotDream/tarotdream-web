1cd666c7a3de3434bdd4cc7d019e718a7a98c15b
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-09-03 00:33:05 +0900

    jun: 메인 페이지 노출 카드 DB 정리 완료

--- src/routes/home/+page.ts
+++ src/routes/home/+page.ts
@@ -3,11 +3,10 @@ import { getDreams } from "$lib/apis/api.js";
 import type { dreamCard } from "$lib/apis/types.js";
 
 //** fetch all dreams */
-//** TODO : currently, trimming dream card which id is smaller then 15. DB needs to be cleaned */
 const fetchDreams = async () => {
   try {
     const card = await getDreams<dreamCard[]>(getAllDreamsURI());
-    return { ...card.data, response : card.data.response.slice(15)};
+    return card.data;
   } catch(err) {
     console.log('ERR : fetching Single Dream / ' + err);
     return null;
