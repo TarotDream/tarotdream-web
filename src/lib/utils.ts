7160de8c7592958e92d357ee0243efff36b05ab2
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-09-03 02:00:04 +0900

    jun: Modal 띄우고 끄기 위한 함수 정의

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,15 +1,20 @@
 import { goto } from '$app/navigation';
 import { moveBack } from './constants/strings';
+import { showModal, modalName } from '$lib/stores';
 
+//** [naviagtor] navigating to specific uri */
 export const navigateTo = (destination: string) => {
 	if(moveBack === destination) history.back();
 	else goto(destination);
 };
 
+//** [navigator] making destination */
 export const destinationAsURI = (menus: string[]) => '/' + menus.join('/');
 
+//** [utility] making number to string */
 export const numberToString = (value : number) : string => value + '';
 
+//** [utility] making timestamptz to YYYY.MM.DD string*/
 export const timestampToY4M2D2 = (timestamptz : string) : string => {
 	const dateObject = new Date(timestamptz);
 
@@ -18,4 +23,16 @@ export const timestampToY4M2D2 = (timestamptz : string) : string => {
 	const day = (dateObject.getDate().toString().padStart(2, '0'));
 
 	return year + "." + month + "." + day;
+}
+
+//** [utility] rendering modal */
+export const mountModal = (name : string) => {
+	showModal.set(true);
+	modalName.set(name);
+}
+
+//** [utility] destroying modal */
+export const destroyModal = () => {
+	showModal.set(false);
+	modalName.set('default');
 }
